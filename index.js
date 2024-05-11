b4b087172baae7947190b3af896c73b0aeeb65f0
santy4747 <72089354+santy4747@users.noreply.github.com>
2024-05-11 17:06:10 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom'
+import App from './app'
+
+// function image(){
+//     return (
+//         <div>
+//             <box-icon name="rocket"></box-icon>
+//         </div>
+//     )
+// }
+
+ReactDOM.render(<App />, document.getElementById('root'))
